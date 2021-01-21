447247d039cc7d929725e9d2d82dba8db532e8c9
Connor Todd <connortodd0304@gmail.com>
2021-01-21 23:09:10 +0000

    added auth to the staff endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
-
-// case-insensitive + non-strict routing
 const router = express.Router();
+const passport = require("passport");
+
+// middleware to pass on the company_id in the req.params
+const passCompanyId = function (req, res, next) {
+  (req.company_id = req.params.company_id), next();
+};
 
 // importing controllers and defining a prefix for each
 router.use("/api/auth", require("./authentication"));
-router.use("/api/:company_id", require("./company"));
-router.use("/api/:company_id/booking", require("./booking"));
-router.use("/api/:company_id/client", require("./client"));
-router.use("/api/:company_id/service", require("./service"));
-router.use("/api/:company_id/staff", require("./staff"));
+router.use(
+  "/api/company/:company_id/staff",
+  passport.authenticate("jwt", { session: false }),
+  passCompanyId,
+  require("./staff")
+);
+router.use("/api/company/:company_id/booking", require("./booking"));
+router.use("/api/company/:company_id/client", require("./client"));
+router.use("/api/company/:company_id/service", require("./service"));
+router.use("/api/company/:company_id", require("./company"));
 
 module.exports = router;
--- controllers/staff.js
+++ controllers/staff.js
@@ -1,17 +1,37 @@
 const express = require("express");
-
-// case-insensitive + non-strict routing
 const router = express.Router();
-
-// Middleware to apply to all routes
-router.use((req, res, next) => {});
+const connection = require("../config.js");
 
 /*--------------------------------------------------------------------------
     Prefix of all routes is defined in index.js = /api/:company_id/staff
 ------------------------------------------------------------------------- */
 
-// GET ALL STAFF MEMBERS (/api/:company_id/staff)
-router.get("/", (req, res, next) => {});
+// GET ALL STAFF MEMBERS (/api/company/:company_id/staff)
+router.get("/", (req, res) => {
+  const company_id = req.company_id;
+  connection.query(
+    "SELECT id, username, firstname, lastname, headshot_path, email_address FROM staff WHERE company_id = ?",
+    [company_id],
+    (err, results) => {
+      console.log(results);
+      if (err) res.status(500).send(err);
+      if (!results.length) {
+        res.status(404).send({ message: "There were no staff found" });
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
+});
+
+// POST a staff member (/api/:company_id/staff)
+router.post("/", (req, res, next) => {});
+
+// PUT staff member (/api/:company_id/staff/:staff_id)
+router.put("/:staff_id", (req, res, next) => {});
+
+// DELETE staff member (/api/:company_id/staff/:staff_id)
+router.delete("/:staff_id", (req, res, next) => {});
 
 // GET all available start time for a specific staff member to provide a specific service
 // on a specific date (having into account existing bookings)
@@ -24,28 +44,11 @@ router.get(
   }
 );
 
-// ----------------
-
-// POST a staff member (/api/:company_id/staff)
-router.post("/", (req, res, next) => {});
-
 // POST specific service provided by specific staff member (/api/:company_id/staff/:staff_id/service)
 router.post("/:staff_id/service", (req, res, next) => {});
-
-// ----------------
-
-// PUT staff member (/api/:company_id/staff/:staff_id)
-router.put("/:staff_id", (req, res, next) => {});
-
-// ----------------
-
-// DELETE staff member (/api/:company_id/staff/:staff_id)
-router.delete("/:staff_id", (req, res, next) => {});
 
 // DELETE specific service provided by specific staff member
 // (/api/:company_id/staff/:staff_id/service/:service_id)
 router.delete("/:staff_id/service/:service_id", (req, res, next) => {});
-
-// ----------------
 
 module.exports = router;
