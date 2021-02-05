f9fcf9f9cfcd9e446d0aa073cb4e1e866be65cf5
Connor Todd <connortodd0304@gmail.com>
2021-02-05 14:07:57 +0000

    return the newly formatted booking after completion of post request

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { bookingsFormatter } = require("../utils/bookings-formatter");
 const connection = require("../config.js");
 
 /*-----------------------------------------------------------------------
@@ -16,56 +17,9 @@ router.get("/:date", (req, res, next) => {
     (err, results) => {
       if (err) res.status(500).send(err);
       // map over each service and then calculate the time for this booking
-      let bookings = results;
-      let bookingsReformatted = [];
+      let bookings = bookingsFormatter(results);
 
-      for (let i = 0; i < bookings.length; i++) {
-        // destructuring properties out of the bookings[i] which are a part of the service
-        // the rest is spread to the variable booking
-        const {
-          booking_service_id,
-          duration_minutes,
-          service_name,
-          ...booking
-        } = bookings[i];
-        // check if booking doesn't exist in bookingsReformatted and then push it there
-        if (
-          !bookingsReformatted.some((element) => element.id === bookings[i].id)
-        ) {
-          // push the booking to the bookingsReformatted array and setup the services array
-          bookingsReformatted.push({
-            ...booking,
-            servicesTotalDuration: duration_minutes,
-            services: [
-              {
-                booking_service_id,
-                service_name,
-              },
-            ],
-          });
-        } else {
-          // the booking already exists within the array so we add the extra service for that booking
-          bookingsReformatted = bookingsReformatted.map((element) =>
-            // here we find the booking which needs the extra service added to its services array
-            element.id === bookings[i].id
-              ? {
-                  ...element,
-                  servicesTotalDuration:
-                    element.servicesTotalDuration + duration_minutes,
-                  services: [
-                    ...element.services,
-                    {
-                      booking_service_id,
-                      service_name,
-                    },
-                  ],
-                }
-              : element
-          );
-        }
-      }
-      // send the array of bookings with the array of services attached to each booking
-      res.status(200).send(bookingsReformatted);
+      res.status(200).send(bookings);
     }
   );
 });
@@ -87,6 +41,16 @@ router.post("/", (req, res, next) => {
         }
       );
     });
+    connection.query(
+      `SELECT  staff.headshot_path as staff_headshot_path, client.firstname as client_first_name, client.lastname as client_last_name, client.phone_number as client_phone_number, service.name as service_name, booking.service_date_time, booking_service.id as booking_service_id, service.duration_minutes, booking.id FROM booking JOIN booking_service ON booking_service.booking_id = booking.id JOIN service ON booking_service.service_id = service.id JOIN staff ON booking.staff_id = staff.id JOIN client ON client.id = booking.client_id WHERE booking.id = ?`,
+      [bookingId],
+      (err, results) => {
+        if (err) res.status(500).send(err);
+        console.log(results);
+        const bookings = bookingsFormatter(results);
+        res.status(200).send(bookings);
+      }
+    );
   });
 });
 
